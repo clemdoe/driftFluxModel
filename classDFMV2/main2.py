c96a746e3600603d6e6571216fcf893de4134d14
clemdoe <clement.huet.2@gmail.com>
2024-08-15 10:56:25 -0400

    testing friction factor

--- classDFMV2/main2.py
+++ classDFMV2/main2.py
@@ -23,10 +23,9 @@ Poro = 0.5655077285
 cote = 0.0157 * np.sqrt(Poro)
 
 DFM1 = DFMclass(nCells, u_inlet, P_outlet, h_inlet, height, fuelRadius, cladRadius, cote, 'FVM', 'base', 'base', 'GEramp', 500000000, 'constant')
+DFM2 = DFMclass(nCells, u_inlet, P_outlet, h_inlet, height, fuelRadius, cladRadius, cote, 'FVM', 'blasius', 'base', 'GEramp', 500000000, 'constant')
 #DFM2 = DFMclass(nCells, u_inlet, P_outlet, h_inlet, height, fuelRadius, cladRadius, waterGap, 'FVM', 'base', 'base', 'modBestion')
 DFM1.resolveDFM()
-
-#DFM2.resolveDFM()
 DFM1.createBoundaryEnthalpy()
 h = DFM1.H[-1]/1000
 eps = DFM1.voidFraction[-1]
@@ -120,7 +119,7 @@ plt.title('Velocity distribution')
 plt.legend()
 
 fig11 = plt.figure()
-plt.plot(z, DFM1.P[-1])
+plt.plot(z, DFM1.P[-1], label="0.0001")
 plt.xlabel('Height (m)')
 plt.ylabel('Pressure (Pa)')
 plt.title('Pressure distribution')
